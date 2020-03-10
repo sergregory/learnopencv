a1ec51119374c457b5e3cc94e09c007265db967e
Grigory Serebryakov <serebryakovgr@gmail.com>
2020-03-10 17:13:31 +0300

    Add receptive field calculation

---
+++ PyTorch-Fully-Convolutional-Image-Classification/receptive_field.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+from FullyConvolutionalResnet18 import FullyConvolutionalResnet18
+
+LAYER_INDEX = 0
+
+def sliding_window_receptive_field_size_hook(module, input, output):
+    global LAYER_INDEX
+
+    stride_prev, receptive_field_prev = get_prev_layer_data(input)
+    stride_current = get_element(module.stride, 0)
+    kernel_size = get_element(module.kernel_size, 0)
+    receptive_field = receptive_field_prev + (kernel_size - 1) * stride_prev
+    stride_total = stride_current * stride_prev
+    output[0, LAYER_INDEX % output.size()[1], 0, 0] = receptive_field
+    output[0, LAYER_INDEX % output.size()[1], 0, 1] = stride_total
+    LAYER_INDEX += 1
+    # print(type(module), stride_current, kernel_size, receptive_field)
+
+def get_element(sequence, index):
+    try:
+        _ = iter(sequence)
+    except TypeError:
+        element = sequence
+    else:
+        element = sequence[index]
+    return element
+
+def get_prev_layer_data(input):
+    input = torch.unique_consecutive(input[0], dim = 1)
+    receptive_field_max_index = int(torch.argmax(input[:, :, 0, 0]).item())
+    receptive_field_prev = int(input[0, receptive_field_max_index, 0, 0].item())
+    stride_prev = int(input[0, receptive_field_max_index, 0, 1].item())
+    return stride_prev, receptive_field_prev
+
+def no_change_receptive_field_size_hook(module, input, output):
+    global LAYER_INDEX
+    stride_prev, receptive_field_prev = get_prev_layer_data(input)
+    output[0, LAYER_INDEX % output.size()[1], 0, 0] = receptive_field_prev
+    output[0, LAYER_INDEX % output.size()[1], 0, 1] = stride_prev
+    LAYER_INDEX += 1
+    # print(type(module), receptive_field_prev)
+
+def get_receptive_filed_calc_by_module(module):
+    mapping = {
+        nn.Conv2d: sliding_window_receptive_field_size_hook,
+        nn.BatchNorm2d: no_change_receptive_field_size_hook,
+        nn.AvgPool2d: sliding_window_receptive_field_size_hook,
+        nn.MaxPool2d: sliding_window_receptive_field_size_hook,
+        nn.ReLU: no_change_receptive_field_size_hook,
+    }
+    return mapping.get(type(module))
+
+def main():
+    model = FullyConvolutionalResnet18().eval()
+    hook_handlers = []
+    for module in model.modules():
+        hook = get_receptive_filed_calc_by_module(module)
+        if hook is not None:
+            hook_handlers.append(module.register_forward_hook(hook))
+        try:
+            nn.init.zeros_(module.weight)
+            nn.init.zeros_(module.bias)
+            nn.init.zeros_(module.running_mean)
+            nn.init.zeros_(module.running_var)
+        except:
+            pass
+
+    # print(model)
+    input = torch.zeros(size=(1, 3, 224 * 2, 224 * 2))
+    input[0, 0, 0, 0] = 1
+    input[0, 0, 0, 1] = 1
+    out = model(input)
+    with torch.no_grad():
+        print('Receptive field size for the pixel in the output feature map:', int(torch.max(out, dim=1)[0][0, 0, 0].item()))
+    for handler in hook_handlers:
+        handler.remove()
+
+if __name__ == "__main__":
+    main()
